0c7b613f51cb5ce196bd996cc8727c866649cd9b
VarenytsiaMykhailo <52725148+VarenytsiaMykhailo@users.noreply.github.com>
2022-04-21 00:13:52 +0300

    technoparkContext\module1\task1\var1

--- technoparkContext/module1/task1/var1/main.cpp
+++ technoparkContext/module1/task1/var1/main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
 
+/*
+Подсчитать кол-во единичных бит в входном числе , стоящих на четных позициях.
+Позиции битов нумеруются с 0.
+
+Необходимо использование битовых операций.
+Использование арифметических операций запрещено.
+
+Формат ввода:
+Входное число лежит в диапазоне 0..2^32-1 и вводится в десятичном виде.
+
+Формат вывода:
+Целое число - количество бит
+
+Примеры ввода in/out:
+1/1
+5/2
+25/2
+*/
+
 unsigned int calcNumOfBitsInEvenPositions(unsigned int);
 
 int main() {
@@ -13,10 +32,8 @@ int main() {
 
 unsigned int calcNumOfBitsInEvenPositions(unsigned int num) {
     unsigned int count = 0;
-    while (num != 0) {
-        if (num & 1) {
-            count++;
-        }
+    while (num) {
+        count += num & 1;
         num = num >> 2;
     }
     return count;
